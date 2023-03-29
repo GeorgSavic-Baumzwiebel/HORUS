b957d071d66cd49d9a737c1b484b2b2ca958e0ee
Georg Savic <98040938+GeorgSavic-Baumzwiebel@users.noreply.github.com>
2023-03-29 18:01:51 +0200

    Oh yeah Bootreihenfolge switched geht

--- test.py
+++ test.py
@@ -8,11 +8,15 @@ from pywinos import WinOSClient
 import winrm
 
 
-def change_boot_priority(ip_address):
-    session = winrm.Session(ip_address, auth=("security", "security"))
-    run = session.run_cmd('hostname')
-    return run.std_out
-
+def change_boot_priority(ip_address,system):
+    session = winrm.Session(ip_address, auth=("junioradmin", "junioradmin"))
+    test = f"bcdedit  | Select-String '{system}' -Context 3,0 | ForEach-Object {{ $_.Context.PreContext[0] -replace '^Bezeichner +\' }}"
+    print(test)
+    run = session.run_ps(f"bcdedit | Select-String '{system}' -Context 3,0 | ForEach-Object {{ $_.Context.PreContext[0] -replace '^identifier +\' }}")
+    id = str(run.std_out)[2:-5]
+    session.run_ps(f"bcedit /default \"{id}\"")
+    session.run_ps("shutdown /r /t 0")
+    return id
 
 
 def trustedhosts(ip_address):
@@ -24,4 +28,4 @@ def multiple_hosts(filename):
         trustedhosts(line)
         change_boot_priority(line)
 
-print(change_boot_priority('10.0.76.6'))
+print(change_boot_priority('10.0.0.18','GNS3-Security'))
