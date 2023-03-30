3d7d2b864dabd262dcbad98cfd2b802c4d8d44a3
Georg Savic <98040938+GeorgSavic-Baumzwiebel@users.noreply.github.com>
2023-03-30 09:20:27 +0200

    Doku von test.py

--- test.py
+++ test.py
@@ -9,9 +9,15 @@ import winrm
 
 
 def change_boot_priority(ip_address,system):
+    """
+    this function is used to change the boot priority of a given host
+    It works by opening up a winrm session with the remote host and then sending ps commands to it to change the boot priority.
+    following that it restarts the system with the "shutdown /r /t 0" command.
+    :param ip_address: The IP address of the remote host
+    :param system: the subsystem description to change to
+    :return: the GUID of the remote host
+    """
     session = winrm.Session(ip_address, auth=("junioradmin", "junioradmin"))
-    test = f"bcdedit  | Select-String '{system}' -Context 3,0 | ForEach-Object {{ $_.Context.PreContext[0] -replace '^Bezeichner +\' }}"
-    print(test)
     run = session.run_ps(f"bcdedit | Select-String '{system}' -Context 3,0 | ForEach-Object {{ $_.Context.PreContext[0] -replace '^identifier +\' }}")
     id = str(run.std_out)[2:-5]
     session.run_ps(f"bcedit /default \"{id}\"")
@@ -20,12 +26,20 @@ def change_boot_priority(ip_address,system):
 
 
 def trustedhosts(ip_address):
+    """
+    this function is used to add a speficied ip address to the list of trusted hosts
+    :param ip_address: The IP address to add to the trusted host file on this machine
+    :return: nothing
+    """
     subprocess.call(
         f"powershell Set-Item WSMan:\localhost\Client\TrustedHosts -Value '{ip_address}' -Concatenate -Force")
 
 def multiple_hosts(filename):
+    """
+    This function is used to iterate through a file containing multiple hosts and change the boot priority on each remote host
+    :param filename: The file name containing the remote hosts seperated by line breaks containing one ip per line
+    :return: nothing
+    """
     for line in filename:
         trustedhosts(line)
         change_boot_priority(line)
-
-print(change_boot_priority('10.0.0.18','GNS3-Security'))
